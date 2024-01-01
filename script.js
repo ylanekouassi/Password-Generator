a2511be3211e07821a545555958230bbfd366668
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-01-01 17:32:59 -0500

    password criteria created

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const passwordGenerator = document.getElementById("password")
 const passwordLength = 12;
- 
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol ="@#$%^&*()_+~|}{[]></-=!?"
