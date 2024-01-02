5fef561c1c74338ca5baf339a7cdbee2605e2d8f
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-01-01 21:05:04 -0500

    random passwords generated successfully

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ const passwordLength = 12;
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
-const symbol ="@#$%^&*()_+~|}{[]></-=!?"
+const symbol = "@#$%^&*()_+~|}{[]></-=!?";
+const result = upperCase + lowerCase + number + symbol;
 
-function createPassword (){
+function createPassword(event) {
+    event.preventDefault();
     let password = "";
-    password += upperCase[Math.floor(Math.random()*upperCase.length)];
-    password += lowerCase[Math.floor(Math.random()*lowerCase.length)];
-    password += number[Math.floor(Math.random()*number.length)];
-    password += symbol[Math.floor(Math.random()*symbol.length)];
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (passwordLength > password.length) {
+        password += result[Math.floor(Math.random() * result.length)];
+    }
+
+    passwordGenerator.value = password;
+
 }
